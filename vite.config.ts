c0a8494a4b4cb15aad007404226485dae63f3133
Abode1919 <ghosenabdo7@gmail.com>
2025-10-02 22:06:50 +0200

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React_SyrianVisions/', // ← bytt til nøyaktig repo-namn
+  base: '/', // ← bytt til nøyaktig repo-namn
 })
