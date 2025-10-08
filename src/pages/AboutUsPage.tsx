33bf8ba69e8ae25ae193597567e92a7d2c74b802
Abode1919 <ghosenabdo7@gmail.com>
2025-10-09 00:44:47 +0200

    yrd

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -105,7 +105,7 @@ const AboutUsPage: React.FC = () => {
 
 
         {/* Mission */}
-        <section className="py-20 bg-primary">
+        <section id="mission" className="py-20 bg-primary">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="grid lg:grid-cols-2 gap-12 items-center">
               <div>
